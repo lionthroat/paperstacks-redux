aebb79d3ebed964debb2c1b73c19ef307f65f051
heatherdiruscio <diruscih@oregonstate.edu>
2020-02-19 14:57:28 -0800

    connect

--- db.py
+++ db.py
@@ -5,8 +5,8 @@ from flaskext.mysql import MySQL
 mysql = MySQL()
 
 # MySQL configurations
-app.config['MYSQL_DATABASE_USER'] = 'root'
-app.config['MYSQL_DATABASE_PASSWORD'] = 'root'
-app.config['MYSQL_DATABASE_DB'] = 'roytuts'
-app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+app.config['MYSQL_DATABASE_USER'] = 'cxeqsk4416lwhuri'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'ch4at87dhgpuv005'
+app.config['MYSQL_DATABASE_DB'] = 'e17gpbo1zl8mbfi9'
+app.config['MYSQL_DATABASE_HOST'] = 'spvunyfm598dw67v.cbetxkdyhwsb.us-east-1.rds.amazonaws.com'
 mysql.init_app(app)
