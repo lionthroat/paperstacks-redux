44266325cb16367268aa5d27f7dc10fad4ed3790
heatherdiruscio <diruscih@oregonstate.edu>
2020-02-18 16:43:20 -0800

    connect db

--- run.py
+++ run.py
@@ -1,7 +1,13 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template, request
+import mysql.connector as jawsdb
 from sample_book_data.bookData import Books
 from sample_book_data.genreData import Genres
+
+
+#!/usr/bin/python
+# https://mariadb.com/resources/blog/how-to-connect-python-programs-to-mariadb/
+jawsdb_connection = jawsdb.connect(host='spvunyfm598dw67v.cbetxkdyhwsb.us-east-1.rds.amazonaws.com', user='cxeqsk4416lwhuri', password='ch4at87dhgpuv005', database='e17gpbo1zl8mbfi9')
+cursor = jawsdb_connection.cursor()
 
 Books = Books()
 Genres = Genres()
