ee40fc0e971b4f1afb18da65549cc18ed94917db
heatherdiruscio <diruscih@oregonstate.edu>
2020-03-11 02:18:23 -0700

    Book page now displays even if Author is Unknown

--- run.py
+++ run.py
@@ -30,12 +30,17 @@ def books():
 
 @app.route('/book/<string:isbn>/')
 def book(isbn):
-    select = "select book.isbn, book.book_title, book.year_published, book.book_description, auth.author_name, auth.author_id, genre.genre_name from Books book join Books_Authors ba on ba.isbn = book.isbn join Authors auth on auth.author_id = ba.author_id join Genres_Books gb on gb.isbn = book.isbn join Genres genre on genre.genre_id = gb.genre_id where book.isbn = " + isbn
+    # Step 1: Fetch Book's information (Returns 1 entry per genre for the book - this is fine!)
+    select = "SELECT book.isbn, book.book_title, book.year_published, book.book_description, genre.genre_name from Books book join Genres_Books gb on gb.isbn = book.isbn join Genres genre on genre.genre_id = gb.genre_id where book.isbn = " + isbn
+    book = fetch(select)
 
-    BookSQL = fetch(select) # Step 1: Fetch Book's information
+    # Step 2: Fetch Book's Author(s) - may be none
+    select = "SELECT auth.author_name, auth.author_id from Authors auth JOIN Books_Authors ba ON ba.author_id = auth.author_id where ba.isbn = " + isbn
+    authors = fetch(select)
 
+    # Step 3: Get Avg Rating (float avg, total # of ratings)
     select = "SELECT AVG(star_rating) AS `average_rating`, COUNT(rate.rating_id) AS `rating_count` FROM Ratings rate WHERE rate.isbn = " + isbn
-    AvgRatingSQL = fetch(select) # Step 2: Get Avg Rating (float avg, total # of ratings)
+    AvgRatingSQL = fetch(select)
 
     # Initialize so that book.html doesn't break expecting these vars
     float_avg = None
@@ -48,18 +53,18 @@ def book(isbn):
         rating_count = AvgRatingSQL[0]['rating_count']
 
     select = "select book.isbn, rate.rating_id, rate.review_id, rate.star_rating, rate.rating_date, rev.review_content from Books book join Ratings rate on rate.isbn = book.isbn join Reviews rev on rev.isbn = rate.isbn where book.isbn = " + isbn
-    ReviewSQL = fetch(select) # Step 3: Fetch Book's Reviews with Ratings
+    ReviewSQL = fetch(select) # Step 4: Fetch Book's Reviews with Ratings
 
     select = "SELECT * FROM Ratings WHERE isbn = " + isbn + " AND review_id IS NULL"
-    RatingSQL = fetch(select) # Step 4: Fetch Book's Ratings that have no Review
+    RatingSQL = fetch(select) # Step 5: Fetch Book's Ratings that have no Review
 
-    # Step 5: For Edit Book Modal
+    # Step 6: For Edit Book Modal
     select = "SELECT Genres.genre_id, Genres.genre_name FROM Genres"
-    genres = fetch(select)
+    all_genres = fetch(select)
     select = "SELECT Authors.author_id, Authors.author_name FROM Authors"
-    authors = fetch(select)
+    all_authors = fetch(select)
 
-    return render_template('book.html', bookresult=BookSQL, reviews=ReviewSQL, ratings=RatingSQL, genres=genres, authors=authors, rating_count=rating_count, int_avg=int_avg, float_avg=float_avg)
+    return render_template('book.html', book=book, authors=authors, reviews=ReviewSQL, ratings=RatingSQL, all_genres=all_genres, all_authors=all_authors, rating_count=rating_count, int_avg=int_avg, float_avg=float_avg)
 
 # Something was changed, redisplay book page and give user message
 @app.route('/book/<string:isbn>/update/<string:code>')
@@ -67,11 +72,17 @@ def book_updated(isbn, code):
     code = int(code)
     code_msg = Messages[code]
 
-    select = "select book.isbn, book.book_title, book.year_published, book.book_description, auth.author_name, auth.author_id, genre.genre_name from Books book join Books_Authors ba on ba.isbn = book.isbn join Authors auth on auth.author_id = ba.author_id join Genres_Books gb on gb.isbn = book.isbn join Genres genre on genre.genre_id = gb.genre_id where book.isbn = " + isbn # do NOT group by isbn or you will suppress multiple genre listings for the same book!!!!
-    BookSQL = fetch(select) # Step 1: Fetch Book's information
+    # Step 1: Fetch Book's information (Returns 1 entry per genre for the book - this is fine!)
+    select = "SELECT book.isbn, book.book_title, book.year_published, book.book_description, genre.genre_name from Books book join Genres_Books gb on gb.isbn = book.isbn join Genres genre on genre.genre_id = gb.genre_id where book.isbn = " + isbn
+    book = fetch(select)
 
+    # Step 2: Fetch Book's Author(s) - may be none
+    select = "SELECT auth.author_name, auth.author_id from Authors auth JOIN Books_Authors ba ON ba.author_id = auth.author_id where ba.isbn = " + isbn
+    authors = fetch(select)
+
+    # Step 3: Get Avg Rating (float avg, total # of ratings)
     select = "SELECT AVG(star_rating) AS `average_rating`, COUNT(rate.rating_id) AS `rating_count` FROM Ratings rate WHERE rate.isbn = " + isbn
-    AvgRatingSQL = fetch(select) # Step 2: Get Avg Rating (float avg, total # of ratings)
+    AvgRatingSQL = fetch(select)
 
     # Initialize so that book.html doesn't break expecting these vars
     float_avg = None
@@ -83,19 +94,19 @@ def book_updated(isbn, code):
         int_avg = round(AvgRatingSQL[0]['average_rating'])
         rating_count = AvgRatingSQL[0]['rating_count']
 
-    select = "select book.isbn, rate.rating_id, rate.review_id, rate.star_rating, rate.rating_date, rev.review_content from Books book join Ratings rate on rate.isbn = book.isbn join Reviews rev on rev.isbn = rate.isbn where book.isbn = " + isbn + " AND rev.rating_id = rate.rating_id AND rate.review_id = rev.review_id"
-    ReviewSQL = fetch(select) # Step 3: Fetch Book's Reviews with Ratings
+    select = "select book.isbn, rate.rating_id, rate.review_id, rate.star_rating, rate.rating_date, rev.review_content from Books book join Ratings rate on rate.isbn = book.isbn join Reviews rev on rev.isbn = rate.isbn where book.isbn = " + isbn
+    ReviewSQL = fetch(select) # Step 4: Fetch Book's Reviews with Ratings
 
     select = "SELECT * FROM Ratings WHERE isbn = " + isbn + " AND review_id IS NULL"
-    RatingSQL = fetch(select) # Step 4: Fetch Book's Ratings that have no Review
+    RatingSQL = fetch(select) # Step 5: Fetch Book's Ratings that have no Review
 
-    # Step 5: For Edit Book Modal
+    # Step 6: For Edit Book Modal
     select = "SELECT Genres.genre_id, Genres.genre_name FROM Genres"
-    genres = fetch(select)
+    all_genres = fetch(select)
     select = "SELECT Authors.author_id, Authors.author_name FROM Authors"
-    authors = fetch(select)
+    all_authors = fetch(select)
 
-    return render_template('book.html', bookresult=BookSQL, reviews=ReviewSQL, ratings=RatingSQL, code=code, genres=genres, authors=authors, code_msg=code_msg, rating_count=rating_count, int_avg=int_avg, float_avg=float_avg)
+    return render_template('book.html', book=book, authors=authors, reviews=ReviewSQL, ratings=RatingSQL, all_genres=all_genres, all_authors=all_authors, rating_count=rating_count, int_avg=int_avg, float_avg=float_avg, code_msg=code_msg, code=code)
 
 @app.route('/add_book', methods=['POST','GET'])
 def add_book():
