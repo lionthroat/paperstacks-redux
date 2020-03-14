4b1e098a8df9b5bf9da233f015eb3b77ba05e8f7
Kayla Smith <50057385+smithkaylar@users.noreply.github.com>
2020-03-13 20:48:12 -0600

    added start of input validation

--- main.py
+++ main.py
@@ -450,5 +450,9 @@ def privacy():
 def page_not_found(e):
     return render_template('404.html'), 404
 
+@app.errorhandler(500)
+def internal_server_error(e):
+	return render_template('500.html'), 500
+
 if __name__ == "__main__":
     app.run()
